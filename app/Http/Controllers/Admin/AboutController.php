bacd8c9e704e925641edf6850a26e08e1c1a3cd9
TaiTran36 <taitt@house3d.org>
2019-12-05 15:12:27 +0700

    fix show list menu

---
+++ app/Http/Controllers/Admin/AboutController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\About;
+use App\Repositories\AboutRepository;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AboutController extends Controller
+{
+    protected  $aboutRepository;
+
+    public function __construct(AboutRepository $aboutRepository)
+    {
+        $this->aboutRepository = $aboutRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin/about/index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function show(About $about)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(About $about)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, About $about)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\About  $about
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(About $about)
+    {
+        //
+    }
+}
---
+++ app/Repositories/CommomRepository.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Repositories;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\Paginator;
+class CommonRepository
+{
+    public $model;
+    function __construct(Model $model){
+        $this->model = $model;
+    }
+    /**
+     * @param int $limit
+     * @param array $criteria
+     * @param bool $sortType
+     * @return mixed
+     */
+    public function fetchAllWithPaginator($limit = 10, $criteria = [], $sortType = false){
+        $query = $this->model->whereExists(function($query) use ($criteria){
+            $query->select('*')->where('is_deleted' , 0);
+            if(!empty($criteria['title'])){
+                $query->where('title_vi', 'like', '%' . $criteria['title'] . '%');
+                $query->orWhere('title_en', 'like', '%' . $criteria['title'] . '%');
+            };
+        });
+        if($sortType){
+            $query->orderBy('sort', $sortType);
+        }
+        return $query->paginate($limit);
+    }
+    /**
+     * Create a new record in the database
+     * @param array $data
+     * @return mixed
+     */
+    public function store($data = []) {
+        return $this->model->create($data);
+    }
+    /**
+     * Update a record in the database
+     * @param array $data
+     * @return mixed
+     */
+    public function update($id, $data = []) {
+        return $this->model::where(['id' => $id])->update($data);
+    }
+    /**
+     * Change sort order for target row and affected row
+     * @param $data
+     * @param $table
+     * @return mixed
+     */
+    public function sort($data, $table){
+        $cases = [];
+        $ids = [];
+        $params = [];
+        foreach ($data as  $row) {
+            $id = (int) $row['id'];
+            $cases[] = "WHEN {$id} then ?";
+            $params[] = $row['sort'];
+            $ids[] = $id;
+        }
+        $ids = implode(',', $ids);
+        $cases = implode(' ', $cases);
+        return \DB::update("UPDATE `" . $table . "` SET `sort` = CASE `id` {$cases} END
+            WHERE `id` in ({$ids})", $params);
+    }
+    /**
+     * @param integer $id
+     * @return mixed
+     */
+    public function fetchByID($id){
+        return $this->model::where('is_deleted', '=', 0)->findOrFail($id);
+    }
+    /**
+     * @param $order
+     * @return mixed
+     */
+    public function fetchByOrder($order){
+        return $this->model::where(['is_deleted' => 0, 'sort' => $order])->get();
+    }
+}
